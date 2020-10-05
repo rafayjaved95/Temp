e0b99c83397cacadb672ae2e02b60ec906dcc992
Abdul Rafay Javed <Rafay@Abduls-MacBook-Pro.local>
2020-10-05 22:51:46 +0500

    5 Oct Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </switch>
+      </section>
     </Fragment>
   </Router>
 );
